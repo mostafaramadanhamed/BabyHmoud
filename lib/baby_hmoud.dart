b699e3a53229b14dae72c15bfc26b34136fbfc15
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-01-27 16:59:53 +0200

    - root app

---
+++ lib/baby_hmoud.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BabyHmoudApp extends StatelessWidget {
+  const BabyHmoudApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,15 +5,15 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:babyhmoud/baby_hmoud.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:babyhmoud/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const BabyHmoudApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
