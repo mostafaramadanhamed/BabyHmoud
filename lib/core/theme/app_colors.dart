0bf50e9da817a56fa0364c0c0ddc81cb444b455d
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-01-27 17:31:10 +0200

    - added app colors - added font weight helper

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryGold = Color(0xFFC9AD7F);
+  static const Color pimaryBrown = Color(0xFF997652);
+  static const Color darkBrown = Color(0xFF654929);
+  static const Color lighterBrown = Color(0xFFF5F5DD);
+  
+}
