eb5cb8a4526d869bb77f66c2ddf11ddbfe641a92
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-01-27 17:26:54 +0200

    - added text styles

---
+++ lib/core/theme/text_styles.dart
@@ -0,0 +1,11 @@
+import 'package:babyhmoud/core/theme/font_weight_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TextStyles {
+  static TextStyle font20SemiBold = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeightHelper.semiBold,
+  );
+  
+}
