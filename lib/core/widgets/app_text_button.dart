946939c1d8301f1b3ac468d8d17282c36fc5ce6c
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-01-27 17:28:04 +0200

    - added app text button

---
+++ lib/core/widgets/app_text_button.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../theme/app_colors.dart';
+import '../theme/text_styles.dart';
+
+
+class AppTextButton extends StatelessWidget {
+  final double? borderRadius;
+  final Color? backgroundColor;
+  final Color? borderColor;
+  final Color? textColor;
+  final double? horizontalPadding;
+  final double? verticalPadding;
+  final double? buttonWidth;
+  final double? buttonHeight;
+  final String buttonText;
+  final TextStyle? textStyle;
+  final VoidCallback onPressed;
+  const AppTextButton({
+    super.key,
+    this.borderRadius,
+    this.backgroundColor,
+    this.horizontalPadding,
+    this.verticalPadding,
+    this.buttonHeight,
+    this.buttonWidth,
+    required this.buttonText,
+    this.textStyle,
+    required this.onPressed,
+    this.borderColor,
+    this.textColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: ButtonStyle(
+        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(borderRadius ?? 4.0.r),
+         
+          ),
+        ),
+        backgroundColor: WidgetStatePropertyAll(
+          backgroundColor ??AppColors.darkBrown,
+        ),
+     
+        padding: WidgetStateProperty.all<EdgeInsets>(
+          EdgeInsets.symmetric(
+            horizontal: horizontalPadding?.w ?? 12.w,
+            vertical: verticalPadding?.h ?? 14.h,
+          ),
+        ),
+        fixedSize: WidgetStateProperty.all(
+          Size(buttonWidth?.w ?? double.maxFinite, buttonHeight ?? 55.h),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        buttonText,
+        style: textStyle ??
+            TextStyles.font20SemiBold.copyWith(
+              color: textColor ?? Colors.white,
+            ),
+      ),
+    );
+  }
+}
