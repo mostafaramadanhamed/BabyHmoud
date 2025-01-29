efa9bd207e72fef8b6299759f18c0b0a83193a82
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-01-29 11:52:02 +0200

    - added lang icon

--- lib/features/onboarding/ui/on_boarding_screen.dart
+++ lib/features/onboarding/ui/on_boarding_screen.dart
@@ -11,46 +11,73 @@ class OnBoardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        // add lunguage button
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          title: GestureDetector(
+            onTap: () {
+              // navigate to language screen
+            },
+            child: Row(
+            
+              children: [
+                12.pw,
+                const Icon(
+                  Icons.language_rounded,
+                  color: Colors.black,
+                ),
+                8.pw,
+                const Text(
+                  'EN',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
         body: Padding(
-      padding: EdgeInsets.symmetric(horizontal: 64.w),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // app logo
-          Image.asset(
-            'assets/images/logo.png',
-            height: 150.h,
+          padding: EdgeInsets.symmetric(horizontal: 64.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // app logo
+              Image.asset(
+                'assets/images/logo.png',
+                height: 150.h,
+              ),
+              100.ph,
+              // login button
+              AppTextButton(
+                buttonText: 'Login',
+                onPressed: () {
+                  // navigate to login screen
+                  context.pushNamed(Routes.login);
+                },
+              ),
+              20.ph,
+              // register button
+              AppTextButton(
+                buttonText: 'Register',
+                onPressed: () {
+                  // navigate to register screen
+                  context.pushNamed(Routes.register);
+                },
+              ),
+              20.ph,
+              // login as guest button
+              AppTextButton(
+                buttonText: 'Login as guest',
+                onPressed: () {
+                  // navigate to home screen
+                  context.pushNamed(Routes.home);
+                },
+              ),
+              20.ph
+            ],
           ),
-          60.ph,
-          // login button
-          AppTextButton(
-            buttonText: 'Login',
-            onPressed: () {
-              // navigate to login screen
-              context.pushNamed(Routes.login);
-            },
-          ),
-          20.ph,
-          // register button
-          AppTextButton(
-            buttonText: 'Register',
-            onPressed: () {
-              // navigate to register screen
-              context.pushNamed(Routes.register);
-            },
-          ),
-          20.ph,
-          // login as guest button
-          AppTextButton(
-            buttonText: 'Login as guest',
-            onPressed: () {
-              // navigate to home screen
-              context.pushNamed(Routes.home);
-            },
-          ),
-          20.ph
-        ],
-      ),
-    ));
+        ));
   }
 }
