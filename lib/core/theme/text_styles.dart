ce3314c3a37eaa47b038acc9825ce05f3fed9c90
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-01-30 10:59:11 +0200

    - new text styles

--- lib/core/theme/text_styles.dart
+++ lib/core/theme/text_styles.dart
@@ -2,6 +2,8 @@ import 'package:babyhmoud/core/theme/font_weight_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'app_colors.dart';
+
 class TextStyles {
   static TextStyle font23SemiBold = TextStyle(
     fontSize: 23.sp,
@@ -10,9 +12,22 @@ class TextStyles {
   static TextStyle font20SemiBold = TextStyle(
     fontSize: 20.sp,
     fontWeight: FontWeightHelper.semiBold,
-  ); static TextStyle font18SemiBold = TextStyle(
+  );
+  // font 18 semi bold
+  static TextStyle font18SemiBold = TextStyle(
     fontSize: 18.sp,
     fontWeight: FontWeightHelper.semiBold,
+  );
+
+  // font 17 black
+  static TextStyle font17Black = TextStyle(
+      fontSize: 17.sp,
+      fontWeight: FontWeightHelper.semiBold,
+      color: Colors.black);
+  // font 15 dark brown
+  static TextStyle font15DarkBrown = TextStyle(
+    fontSize: 15.sp,
+    color: AppColors.darkBrown,
   );
   // font 14 regular grey
   static TextStyle font14RegularGrey = TextStyle(
@@ -37,5 +52,4 @@ class TextStyles {
     fontSize: 12.sp,
     fontWeight: FontWeightHelper.regular,
   );
-  
 }
