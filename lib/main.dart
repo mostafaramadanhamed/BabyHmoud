f3ad67116dfffa81150582a3e58517ea00be4ebd
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-02-02 10:22:09 +0200

    - add shopify configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:babyhmoud/core/helper/app_secret.dart';
 import 'package:babyhmoud/core/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopify_flutter/shopify_flutter.dart';
 
 import 'baby_hmoud.dart';
 
@@ -8,10 +10,16 @@ import 'baby_hmoud.dart';
 /// It initializes the app by calling `runApp` with `BabyHmoudApp`,
 /// which sets up the application with the provided `AppRouter`.
 
-void main() async{
-    WidgetsFlutterBinding.ensureInitialized();
-  
-  await ScreenUtil.ensureScreenSize();
-runApp( BabyHmoudApp(appRouter: AppRouter(),));
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
+  await ScreenUtil.ensureScreenSize();
+  ShopifyConfig.setConfig(
+    storefrontAccessToken: AppSecret.storeFrontApiKey,
+    storeUrl: AppSecret.storeUrl,
+  );
+  
+  runApp(BabyHmoudApp(
+    appRouter: AppRouter(),
+  ));
+}
